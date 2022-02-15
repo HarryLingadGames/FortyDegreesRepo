8fafd7ff0f2b90ee65a953780d9d47ecab457289
Harry Lingad <hlingad@getsolo.com>
2022-02-15 10:25:04 +0800

    First commit

---
+++ ColorApp/ColorApp/ColorTableViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ColorTableViewController.swift
+//  ColorApp
+//
+//  Created by Harry Lingad on 2/15/22.
+//
+
+import UIKit
+
+class ColorTableViewController: UITableViewController {
+  
+    let colors = [UIColor.red, UIColor.blue, UIColor.green]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      tableView.register(ColorTableViewCell.self, forCellReuseIdentifier: "ColorTableViewCell")
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ColorTableViewCell", for: indexPath) as! ColorTableViewCell
+        cell.backgroundColor = colors[indexPath.row]
+        // Configure the cell...
+
+        return cell
+    }
+    
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let colorViewController = (self.storyboard?.instantiateViewController(identifier: "ViewController"))! as ViewController
+    colorViewController.myColor = colors[indexPath.row]
+    navigationController?.pushViewController(colorViewController, animated: true)
+  }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ColorApp/ColorApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ColorApp
+//
+//  Created by Harry Lingad on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  var myColor: UIColor?
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = myColor
+  }
+
+
+}
+
